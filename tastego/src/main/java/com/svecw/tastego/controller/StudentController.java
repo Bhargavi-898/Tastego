a4a84cf93632429f6dd5fa36c586e7ec48a28511
Bhargavi-898 <sudhabattulasaibhargavi@gmail.com>
2026-04-18 20:50:30 +0530

    updated project

--- tastego/src/main/java/com/svecw/tastego/controller/StudentController.java
+++ tastego/src/main/java/com/svecw/tastego/controller/StudentController.java
@@ -128,15 +128,18 @@ public class StudentController {
                 );
             }
 
-            order.setMenuItemNames(items);
+           order.setMenuItemNames(items);
 
-            order.setQuantity(quantity);
+order.setQuantity(quantity);
+
+// ✅ correct payment image URL
 String imageUrl =
         "https://tastego-production.up.railway.app/api/scanner/view/"
         + scannerFileName;
 
-order.setPaymentScreenshot(imageUrl);     
-       order.setStatus("PENDING");
+order.setPaymentScreenshot(imageUrl);
+
+order.setStatus("PENDING");
             order.setTokenNumber(null);
             order.setDate(LocalDate.now());
             order.setOrderTime(LocalDateTime.now());
